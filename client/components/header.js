839280077ca9f4cd0150fc4189c698b03a6c9fa0
Lorenze Jay <Lorenzejay@gmail.com>
2021-03-26 19:28:32 -0700

    layout adjusted for sidebar and header with button to trigger sidebar appearance

--- client/components/header.js
+++ client/components/header.js
@@ -1,21 +1,40 @@
 import Link from "next/link";
-const Header = () => {
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../redux/Actions/userActions";
+import { FaBars } from "react-icons/fa";
+const Header = ({ setSidebarOpen, sidebarOpen }) => {
+  const dispatch = useDispatch();
+  const userLogin = useSelector((state) => state.userLogin);
+  const { loading, userInfo, error } = userLogin;
+
   return (
     <header className="flex justify-between items-center h-24 px-10 text-white">
-      <Link href="/">
+      {/* <Link href="/">
         <h2 className="text-3xl font-bold">FREELANCIN</h2>
-      </Link>
-      <ul className="flex justify-between items-center w-48 ">
-        <li>
-          <Link href="/signin">Sign In</Link>
-        </li>
-        <li>
-          <Link href="/signup">Sign Up</Link>
-        </li>
-        <li>
-          <Link href="/projects">Projects</Link>
-        </li>
-      </ul>
+      </Link> */}
+      <button onClick={() => setSidebarOpen(!sidebarOpen)} className="outline-none border-0">
+        <FaBars size={30} />
+      </button>
+      {!userInfo && (
+        <ul className="flex justify-between items-center w-64 ">
+          <li>
+            <Link href="/signin">Sign In</Link>
+          </li>
+          <li>
+            <Link href="/signup">Sign Up</Link>
+          </li>
+        </ul>
+      )}
+      {userInfo && (
+        <ul className="flex justify-between items-center w-64 ">
+          <li>
+            <button onClick={() => dispatch(logout())}>Logout</button>
+          </li>
+          <li>
+            <Link href="/projects">Projects</Link>
+          </li>
+        </ul>
+      )}
     </header>
   );
 };
