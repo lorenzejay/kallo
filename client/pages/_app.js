37d71f6a73eb9be86f5f401999d28794dd4dba4f
Lorenze Jay <Lorenzejay@gmail.com>
2021-03-26 19:31:29 -0700

    wrap app with redux

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,6 +1,12 @@
 import "../styles/globals.css";
+import { Provider } from "react-redux";
+import store from "../redux/store";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />;
+    </Provider>
+  );
 }
 
 export default MyApp;
