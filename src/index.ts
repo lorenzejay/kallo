4de8e697d876359809c2bc6f065609188088aa42
Lorenze Jay Hernandez <lorenzejaytech@gmail.com>
2022-01-02 11:54:32 -0800

    updated routes

--- src/index.ts
+++ src/index.ts
@@ -16,22 +16,28 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-//routes
-app.use("/api/users", userRouter);
-app.use("/api/projects", ProjectRouter);
-app.use("/api/todos", todoRouter);
-app.use("/api/columns", columnRouter);
-app.use("/api/tasks", taskRouter);
-app.use("/api/tags", tagRouter);
-app.use("/api/sharing", sharingRouter);
-
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/out/")));
-
+  //routes
+  app.use("/api/users", userRouter);
+  app.use("/api/projects", ProjectRouter);
+  app.use("/api/todos", todoRouter);
+  app.use("/api/columns", columnRouter);
+  app.use("/api/tasks", taskRouter);
+  app.use("/api/tags", tagRouter);
+  app.use("/api/sharing", sharingRouter);
   app.get("*", (_, res) => {
     res.sendFile(path.resolve(__dirname, "../client/out/index.html"));
   });
 } else {
+  //routes
+  app.use("/api/users", userRouter);
+  app.use("/api/projects", ProjectRouter);
+  app.use("/api/todos", todoRouter);
+  app.use("/api/columns", columnRouter);
+  app.use("/api/tasks", taskRouter);
+  app.use("/api/tags", tagRouter);
+  app.use("/api/sharing", sharingRouter);
   app.get("/", (_, res: Response) => {
     res.send("Server Started");
   });
