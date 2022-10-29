a2d60272a1434509ff0c7765d0221a2e6721b4c1
Lorenze Jay <lorenzejaytech@gmail.com>
2022-10-29 11:16:31 -0700

    unneeded showing shared users on project card

--- client/components/projectCard.tsx
+++ client/components/projectCard.tsx
@@ -1,21 +1,12 @@
-import axios from "axios";
 import Link from "next/link";
-import { useContext, useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import { useContext } from "react";
 import { DarkModeContext } from "../context/darkModeContext";
-import { configWithToken } from "../functions";
-import { RootState } from "../redux/store";
 
 type ProjectCardProps = {
   projectId: string;
   title: string;
   headerImg: string;
   isPrivate: boolean;
-};
-
-type SharedUsers = {
-  user_id: string;
-  username: string;
 };
 
 const ProjectCard = ({
@@ -25,28 +16,6 @@ const ProjectCard = ({
   isPrivate,
 }: ProjectCardProps) => {
   const { isDarkMode } = useContext(DarkModeContext);
-  const [sharedUsers, setSharedUsers] = useState<SharedUsers[]>();
-  const userLogin = useSelector((state: RootState) => state.userLogin);
-  const { userInfo } = userLogin;
-
-  const getSharedUsers = async () => {
-    try {
-      if (!userInfo || !projectId || !userInfo.token) return;
-
-      const config = configWithToken(userInfo.token);
-      const { data } = await axios.get(
-        `/api/projects/shared-users/${projectId}`,
-        config
-      );
-      setSharedUsers(data);
-    } catch (error) {
-      throw error;
-    }
-  };
-
-  useEffect(() => {
-    getSharedUsers();
-  }, []);
 
   return (
     <Link href={`/projects/${projectId}`} key={projectId}>
@@ -73,18 +42,6 @@ const ProjectCard = ({
               public
             </div>
           )}
-        </div>
-        <div className="mt-3 flex flex-grow justify-start ">
-          {/* {sharedUsers &&
-            sharedUsers.length !== 0 &&
-            sharedUsers.map((user, i) => (
-              <p
-                key={i}
-                className=" bg-blue-125 rounded-full texdt-white-175  mr-3 p-1 w-9 h-9 flex justify-center items-center text-xl font-medium"
-              >
-                {user.username.substring(0, 1)}
-              </p>
-            ))} */}
         </div>
       </div>
     </Link>
--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -4,7 +4,6 @@ import store from "../redux/store";
 import { ReactPropTypes } from "react";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "../utils/queryClient";
-import { AuthProvider } from "../hooks/useAuth";
 export default function MyApp({
   Component,
   pageProps,
@@ -14,13 +13,9 @@ export default function MyApp({
 }) {
   return (
     <QueryClientProvider client={queryClient}>
-      {/* <AuthProvider> */}
-      <Provider store={store}>
-        {/* <ThemeProvider> */}
-        <Component {...pageProps} />
-        {/* </ThemeProvider> */}
-      </Provider>
-      {/* </AuthProvider> */}
+      {/* <Provider store={store}> */}
+      <Component {...pageProps} />
+      {/* </Provider> */}
     </QueryClientProvider>
   );
 }
