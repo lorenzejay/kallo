448bb4c95db7bd230ef30c8efdf70a975cb76b61
Lorenze Jay <lorenzejaytech@gmail.com>
2022-11-05 20:35:22 -0700

    better hooks & protected route wrapper

--- components/Protected.tsx
+++ components/Protected.tsx
@@ -1,31 +1,25 @@
 import { useRouter } from "next/router";
 import React, { useEffect } from "react";
 import useUser from "../hooks/useUser";
+import Layout from "./layout";
 import Loader from "./loader";
 
 export default function ProtectedWrapper({ children }: { children: any }) {
   const router = useRouter();
   const { isLoading, isError, data: user } = useUser();
   useEffect(() => {
-    if (!user && !isLoading) {
+    if ((!user && !isLoading) || (!user && isError)) {
       router.push("/signin");
     }
-  }, [router, user]);
+  }, [router, user, isError]);
 
   if (isLoading) {
     return (
-      <div className="h-screen grid place-items-center">
-        <Loader size="w-[200px] h-[200px]" />
-      </div>
-    );
-  }
-
-  if (isError) {
-    router.push("/signin");
-    return (
-      <div className="h-screen grid place-items-center">
-        <Loader size="w-[200px] h-[200px]" />
-      </div>
+      <Layout>
+        <div className="h-screen grid place-items-center">
+          <Loader size="w-[200px] h-[200px]" />
+        </div>
+      </Layout>
     );
   }
 
--- hooks/useUser.ts
+++ hooks/useUser.ts
@@ -3,6 +3,7 @@ import supabase from "../utils/supabaseClient";
 
 const getUser = async (userId: string | undefined) => {
   if (!userId) throw Error("No user");
+
   const { data, error } = await supabase
     .from("users")
     .select("*")
@@ -16,7 +17,5 @@ const getUser = async (userId: string | undefined) => {
 
 export default function useUser() {
   const user = supabase.auth.user();
-  return useQuery(["user"], () => getUser(user?.id), {
-    retry: 1,
-  });
+  return useQuery(["user"], () => getUser(user?.id));
 }
